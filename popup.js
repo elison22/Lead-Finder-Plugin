2a1577f70caf5f91091190e9f279e41666132a2d
Brandt Elison <belison@simplifile.com>
2017-04-04 01:42:29 -0600

    removed original sample code and added some functions that we're more likely to use.

--- popup.js
+++ popup.js
@@ -1,140 +1,69 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
+var toScan;
+var leads = [];
+
+function ScanInfo(scanId, scanData, relationship, originId) {
+
+  return {
+    scanId: scanId,
+    scanData: scanData,
+    relationship: relationship,  // options are: initial, son, daughter, husband, wife, mother, father
+    originId: originId
+  };
+
+}
+
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
+
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var tab = tabs[0];
     var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
     callback(url);
+
   });
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
+function getPersonData(personId, callback) {
+
+  jQuery.get("https://familysearch.org/tree-data/portrait-pedigree/bowtie/"+personId+"?includeMarriages=true&includePhotos=false&includeSources=false&locale=en HTTP/1.1")
+    .done(function(data) {
+      callback(personId, data);
+    });
+
 }
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+function scanPerson(personId, data) {
+
+  var coupleInfo = data.data;
+  console.log(coupleInfo);
+
+  // check children
+
+  // check spouse
+  // check parents
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  // getCurrentTabUrl(function(url) {
-  //   // Put the image URL in Google search.
-  //   renderStatus('Performing Google Image search for ' + url);
 
-  //   getImageUrl(url, function(imageUrl, width, height) {
-
-  //     renderStatus('Search term: ' + url + '\n' +
-  //         'Google image search result: ' + imageUrl);
-  //     var imageResult = document.getElementById('image-result');
-  //     // Explicitly set the width/height to minimize the number of reflows. For
-  //     // a single image, this does not matter, but if you're going to embed
-  //     // multiple external images in your page, then the absence of width/height
-  //     // attributes causes the popup to resize multiple times.
-  //     imageResult.width = width;
-  //     imageResult.height = height;
-  //     imageResult.src = imageUrl;
-  //     imageResult.hidden = false;
-
-  //   }, function(errorMessage) {
-  //     renderStatus('Cannot display image. ' + errorMessage);
-  //   });
-  // });
   getCurrentTabUrl(function(url) {
 
     var focusUser = url.match(/.{4}-.{3}/g)[0];
+    var initialUser = ScanInfo(focusUser, null, "initial");
 
-    var baseURL = url.match(/^.+?[^\/:](?=[?\/]|$)/g)[0];
-
-    console.log("here we go with " + focusUser);
-    console.log("for reals");
-    try{
-      jQuery.get(baseURL + "/tree-data/portrait-pedigree/bowtie/"+focusUser+"?includeMarriages=true&includePhotos=true&includeSources=true&locale=en HTTP/1.1")
-      .done(function(data) {
-        console.log(data);
-        console.log(_.keys(data.data));
-        console.log(data.data.husband.gender);
-      }).fail(function() {
-        console.log("error");
-      });
-    } catch (error) {
-      console.log(error.message);
-    }
+    getPersonData(focusUser, scanPerson);
 
   });  
 });
+
+
+
+
